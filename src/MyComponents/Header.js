59a8db32c815e03ceb418326907d0575ddb56778
@dhaivat_kyada_Ebiz <dhaivat.kyada@esparkbizmail.com>
2021-06-28 14:02:15 +0530

    homepage error

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -24,7 +24,7 @@ export default function Header(props) {
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
               <li className='nav-item'>
-                <Link className='nav-link active' aria-current='page' to='/'>
+                <Link className='nav-link active' to='/'>
                   Home
                 </Link>
               </li>

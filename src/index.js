33aacac729183c755a5c3c911af551fc28b6d6b8
@dhaivat_kyada_Ebiz <dhaivat.kyada@esparkbizmail.com>
2021-06-28 14:11:27 +0530

    setting homepage

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* 
       <BrowserRouter></BrowserRouter>
+      <HashRouter></HashRouter>
      */}
-    <HashRouter>
+   
     <App />
-    </HashRouter>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
